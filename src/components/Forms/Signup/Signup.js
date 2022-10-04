6a67471b3f570b144dbde230f3d5dc4531b9dfde
Pericles001 <periclesadjovi@gmail.com>
2022-10-04 17:13:29 +0100

    Edit signup component

--- src/components/Forms/Signup/Signup.js
+++ src/components/Forms/Signup/Signup.js
@@ -5,10 +5,42 @@ import {Link} from "react-router-dom";
 import {useLocation, useNavigate} from "react-router";
 
 export function Signup() {
+    const [redStyle, setRedStyle] = React.useState("");
+    const [firstname, setFirstname] = React.useState("");
+    const [lastname, setLastname] = React.useState("");
+    const [username, setUsername] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+ const [emailMsg, setEmailMsg] = React.useState("We'll never share your email with anyone else.");
+    const [passwordMsg, setPasswordMsg] = React.useState("We'll never share your password with anyone else.");
     const navigate = useNavigate();
-    const location = useLocation();
     const toLog = () => {
-        navigate('/Quizreo_frontend/build/');
+       window.location.href = "https://plusdeca.fr";
+    }
+
+    const handleSubmit = () => {
+        try {
+            if (firstname.length === 0 || lastname.length === 0 || username.length === 0 || email.length === 0 || password.length === 0) {
+                setRedStyle("text-danger");
+
+                if (email.length === 0) {
+                    setEmailMsg("Email is required");
+                } else if (email.length !== 0) {
+                    setEmailMsg("");
+                }
+                if (password.length === 0) {
+                    setPasswordMsg("Password is required");
+                } else if (password.length !== 0) {
+                    setPasswordMsg("");
+                }
+            } else {
+                navigate('/dashboard')
+            }
+        } catch (e) {
+            console.log(e);
+        } finally {
+            console.log(firstname, lastname, username, email, password);
+        }
     }
 
     return (
@@ -17,38 +49,30 @@ export function Signup() {
                 <Form className="mx-auto col-md-4 shadow"
                       style={{backgroundColor: "#D9D9D9", marginTop: "100px", marginBottom: "100px"}}>
                     <h4 className="text-center fw-semibold mt-3 mb-3" style={{cursor: "Pointer"}}
-                        onClick={toLog}>Quizreo</h4>
-                    <Form.Group controlId="formBasicFirstName" className="mt-3">
-                        <Form.Label>First Name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter First Name"/>
-                    </Form.Group>
+                        onClick={toLog}>PlusDeCA</h4>
 
-                    <Form.Group controlId="formBasicLastName" className="mt-3">
-                        <Form.Label>Last Name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter Last Name"/>
-                    </Form.Group>
-
-                    <Form.Group controlId="formBasicSurName" className="mt-3">
-                        <Form.Label>User name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter User Name"/>
-                    </Form.Group>
 
                     <Form.Group controlId="formBasicEmail" className="mt-3">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email"/>
+                        <Form.Control type="email" placeholder="Enter email"
+                                      onChange={(e) => setEmail(e.target.value)}/>
+                        <Form.Text className={"text-muted text-justify " + redStyle}>
+                            {emailMsg}
+                        </Form.Text>
 
                     </Form.Group>
                     <Form.Group controlId="formBasicPassword" className="mt-3">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Enter password"/>
-
+                        <Form.Control type="password" placeholder="Enter password"
+                                      onChange={(e) => setPassword(e.target.value)}/>
+                        <Form.Text className={"text-muted text-justify " + redStyle}>
+                            {passwordMsg}
+                        </Form.Text>
                     </Form.Group>
-                    <Button className="mt-3 mb-3" variant="secondary" size="lg" onClick={() => {
-                        navigate('/Quizreo_frontend/build/dashboard')
-                    }}>
+                    <Button className="mt-3 mb-3" variant="secondary" size="lg" onClick={handleSubmit}>
                         Signup
                     </Button>
-                    <Link className="d-block mt-3 mb-3 text-decoration-none" to="/Quizreo_frontend/build/login">Login here</Link>
+                    <Link className="d-block mt-3 mb-3 text-decoration-none" to="/">Login here</Link>
                 </Form>
             </div>
         </div>
