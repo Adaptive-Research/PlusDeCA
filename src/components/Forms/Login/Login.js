4ea23f0c1bbcb6defbdf36f493d6bbb5c388da61
Pericles001 <periclesadjovi@gmail.com>
2022-10-05 16:09:18 +0100

    Update Login.js - Set email validation control method - Set authentication checker method

--- src/components/Forms/Login/Login.js
+++ src/components/Forms/Login/Login.js
@@ -2,27 +2,77 @@ import React, {useState} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Button, Form} from 'react-bootstrap';
 import {Link} from "react-router-dom";
-import {useLocation, useNavigate} from "react-router";
+import {useNavigate} from "react-router";
+import axios from 'axios';
 
 export function Login() {
+    const [token, setToken] = useState([]);
     const [redStyle, setRedStyle] = useState("");
-    const [UsrMsg, setUsrMsg] = useState("We'll never share your username with anyone else.");
+    const [UsrMsg, setUsrMsg] = useState("We'll never share your email with anyone else.");
     const [passMsg, setPassMsg] = useState("We'll never share your password with anyone else.");
-    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
     const toLog = () => {
         window.location.href = "https://plusdeca.fr";
     }
+
+
+    const checkEmail = (mail) => {
+        let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        if (validRegex.test(mail)) {
+            return true;
+        }
+        return false;
+    }
+
+    const checkAUth = (mail, pass) => {
+        {/*launch a post request to check if user inputs are correects and store the given token to authenticate user*/
+        }
+        const url = "http://78.249.128.56:8001/API/Login";
+
+        axios.post(url, {
+            Submit: 1,
+            Email: mail,
+            Password: pass
+        }, {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            }
+        }).then(
+            (response) => {
+                console.log(response);
+                if () {
+                    setRedStyle("red");
+                    setUsrMsg("Wrong email or password");
+                    setPassMsg("Wrong email or password");
+                } else {
+                    token.push(response.data);
+                    console.log("User logged in", token);
+                    navigate("/dashboard");
+                }
+            }
+        ).catch(
+            (error) => {
+                console.log(error);
+                setRedStyle("red");
+                setUsrMsg("Email or password is incorrect");
+                setPassMsg("Email or password is incorrect");
+            });
+    }
+
     const handleSubmit = () => {
         try {
-            console.log(username, password);
-            if (username.length === 0 || password.length === 0) {
+            console.log(email, password);
+            if (email.length === 0 || password.length === 0) {
                 setRedStyle("text-danger");
-                if (username.length === 0) {
-                    setUsrMsg("Username is required");
-                } else if (username.length !== 0) {
-                    setUsrMsg("We'll never share your username with anyone else.");
+                if (email.length === 0) {
+                    setUsrMsg("Email is required");
+                }else if (!checkEmail(email)) {
+                    setUsrMsg("Email is not valid");
+                }
+                else if (email.length !== 0) {
+                    setUsrMsg("We'll never share your email with anyone else.");
                 }
                 if (password.length === 0) {
                     setPassMsg("Password is required");
@@ -30,12 +80,12 @@ export function Login() {
                     setPassMsg("We'll never share your password with anyone else.");
                 }
             } else {
-                navigate('/dashboard')
+                checkAUth(email, password);
             }
         } catch (e) {
             console.log(e);
         } finally {
-            console.log(username, password);
+            console.log(email, password);
         }
     }
 
@@ -45,10 +95,10 @@ export function Login() {
                 <Form className="mt-5 mx-auto col-md-4 shadow" style={{backgroundColor: "#D9D9D9"}}>
                     <h4 className="text-center fw-semibold mt-3 mb-3" style={{cursor: "Pointer"}}
                         onClick={toLog}>PlusDeCA</h4>
-                    <Form.Group controlId="formBasicUsername" className="mt-3">
-                        <Form.Label>Username</Form.Label>
-                        <Form.Control type="text" placeholder="Enter username"
-                                      onChange={(e) => setUsername(e.target.value)}/>
+                    <Form.Group controlId="formBasicEmail" className="mt-3">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email"
+                                      onChange={(e) => setEmail(e.target.value)}/>
                         <Form.Text className={"text-muted text-justify " + redStyle}>
                             {UsrMsg}
                         </Form.Text>
